5e3fa506c7a0e2c8aa668f17a2accaef88ad6e22
Tayyaba <tayyabasajid.ai@gmail.com>
2024-05-11 19:14:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.magentaBright("\n \t \t \t \t Tayyaba's Word Counter \n"));
+let input = await inquirer.prompt({
+    name: "data",
+    message: chalk.magenta("Enter your sentence/ paragraph \n"),
+    type: "input"
+});
+let data = input.data.trim().split(" ");
+let wordcount = data.length;
+console.log(chalk.bold.magenta(`Word Count: ${chalk.bold.magentaBright(wordcount)}`));
